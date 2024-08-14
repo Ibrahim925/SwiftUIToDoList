bbdebebf06ea1629fa13c5c0c8b5e3bb6f63ac79
ibrahim925 <ikb1703@gmail.com>
2024-08-14 10:15:11 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ibrahim Khawar on 2024-08-14.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
